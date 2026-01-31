917ed13d8183fa805ce6785e4a4bb733dbe315ba
AlphaMikey23 <AlphaMikey23>
2026-01-31 11:24:46 +0530

    Fixed button2

--- javascript.js
+++ javascript.js
@@ -1,12 +1,17 @@
-function updateName() {
+function updateNamePlayer1() {
     const name = prompt("Enter your name:");
     button.textContent = `Player 1: ${name}`;
+}
+
+function updateNamePlayer2() {
+    const name = prompt("Enter your name:");
+    button2.textContent = `Player 2: ${name}`;
 }
 
 
 const button = document.getElementById("player1");
 const button2 = document.getElementById("player2");
 
-button.addEventListener("click",updateName);
-button2.addEventListener("click",updateName);
+button.addEventListener("click",updateNamePlayer1);
+button2.addEventListener("click",updateNamePlayer2);
 
